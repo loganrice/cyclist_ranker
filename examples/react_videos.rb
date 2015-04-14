1d1b3bcb69ceaea50d192e397f69ce0c9e6ed529
Logan Rice <loganrice72@gmail.com>
2015-04-14 16:54:45 -0700

    updated react example to not insert directories

--- examples/react_videos.rb
+++ examples/react_videos.rb
@@ -2,11 +2,11 @@ require 'mechanize'
 require 'pry'
 
 BASE_URL = "https://egghead.io"
+EMAIL = "EMAIL"
+PASSWORD = "PASSWORD"
+
 agent = Mechanize.new
 agent.user_agent_alias = 'iPhone'
-
-EMAIL = "youremail@example.com"
-PASSWORD = "PASSWORD"
 
 def say(msg, important = false)
   puts "" if important
@@ -24,8 +24,8 @@ def next?
 end
 
 agent.get(BASE_URL + "/users/sign_in") do |page|
+
   say "Signing in ..."
-  
   my_page = page.form_with(:action => "/users/sign_in") do |f|
     f.field_with(:id => "user_email").value = EMAIL
     f.field_with(:id => "user_password").value = PASSWORD
@@ -39,49 +39,45 @@ agent.get(BASE_URL + "/users/sign_in") do |page|
   say "Filtering lessons by oldest first"
   react_page = agent.click(react_page.link_with(:text => /Oldest first/))
 
-  page_numbers = agent.page.parser.css('ul.pagination a').map(&:text)
-  page_numbers = page_numbers.map(&:to_i).uniq - [0]
+  say "Counting page numbers"
+  pagination_text = agent.page.parser.css('ul.pagination a').map(&:text)
+  page_numbers = pagination_text.map(&:to_i).uniq - [0]
 
-  page_numbers.each do |page_number| 
+  video_counter = 1
+  page_number = 1
+
+  while page_number <= page_numbers.count + 1 
     agent.page.parser.css('ul.series-lessons-list > a').each do |lesson|
       lesson_title = lesson.css('h5').text rescue nil
 
       lesson_title ? say("Found lesson: " + lesson_title, true) : next
-      dir_name = lesson_title.gsub( /\W/, '_')
+      file_name = lesson_title.gsub( /\W/, '_')
 
-      if Dir.exists?(dir_name)
-        say "#{dir_name} already exists, skipping..."
+      if File.exists?("#{file_name}.mp4")
+        say "#{file_name} already exists, skipping..."
         next
       else
-        say "creating dir: " + dir_name
-        Dir.mkdir(dir_name)
+        say "creating file: " + file_name + "in dir: " + `pwd`
       end
-
-      Dir.chdir(dir_name)
-      say "in dir: " + `pwd`
 
       url = lesson.attributes["href"].value
 
       if url
         download_page = agent.get(BASE_URL + url)
-        say "downloading files at: " + download_page.title
-        download_page.links_with(id: /clicker1/).each do |link|
-          url = link.href 
-          say "downloading... " + url
-          agent.get(url).save_as "#{download_page.title}.mp4"
-        end
+        say "downloading file at: " + download_page.title
+        link = download_page.link_with(id: /clicker1/)
+        agent.get(link.href).save_as "#{file_name}.mp4"
       else
         next
       end
-      
-      Dir.chdir('..')
-      say "back out: " + `pwd`
     end
 
     say "Going to page #{page_number}"
-    react_page = agent.click(react_page.link_with(text: "#{page_number}"))
-    binding.pry
-    
+    page_number += 1
+    binding.pry if page_number == 4
+    react_page = agent.click(react_page.link_with(text: "#{page_number}")) rescue nil 
   end
+
+  say "Download complete"
 end
 
